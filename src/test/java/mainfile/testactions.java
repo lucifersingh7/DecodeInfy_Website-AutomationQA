e63df0cd01572a31cdb818d3de991640b042613e
lucifersingh7 <sahil.singhv18@gmail.com>
2026-09-21 15:30:08 +0530

    Scroll down and switching tabs logic added

--- src/test/java/mainfile/testactions.java
+++ src/test/java/mainfile/testactions.java
@@ -1,5 +1,6 @@
 package mainfile;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.JavascriptExecutor;
@@ -20,13 +21,25 @@ public class testactions {
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		driver.get("https://decodeinfy.in/");   //redirecting to URL
 		
+		String parentWindow = driver.getWindowHandle(); // getting the unique Id of Parent window
+		
 	    pages.homepage.instagramBtn(driver);
-	    JavascriptExecutor js = (JavascriptExecutor) driver;
+	    JavascriptExecutor js = (JavascriptExecutor) driver;  // scrolling down to an WebElement using JS
 	    js.executeScript("arguments[0].scrollIntoView();", pages.homepage.instagramBtn(driver));
 	    Thread.sleep(3000);
 	    pages.homepage.instagramBtn(driver).click();
 	    Thread.sleep(3000);
-			
+	    
+	    
+	    Set<String> Allvalues = driver.getWindowHandles();
+	    for(String windowId: Allvalues ) {
+	    	System.out.println("Window title is: "+windowId);
+	    	Thread.sleep(2000);
+	    }
+	    
+		driver.switchTo().window(parentWindow); // switching back to Parent Window
+	    
+	    Thread.sleep(2000);
 		driver.quit();
 		
 	 }
